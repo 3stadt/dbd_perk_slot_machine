e6acb68da7c9d7c6d61f844d119d6d1f8c8c691a
3stadt <3stadt@users.noreply.github.com>
2019-02-15 18:23:30 +0100

    add Survivor skeleton page

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Survivor from './views/Survivor.vue'
 
 Vue.use(Router)
 
---
+++ src/views/Survivor.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Survivor Roulette</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Survivor'
+}
+</script>
+
+<style scoped>
+
+</style>
