b9d841b24135f39104725eb0a1d01a4fdadb4a01
3stadt <3stadt@users.noreply.github.com>
2021-11-09 23:36:39 +0100

    prepare for 3-digit perk names

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -230,7 +230,7 @@ export default {
     // make sure array keys match the ids in file name. TODO maybe make sure no key is reassigned because of naming issues
     for (let i = 0, sLen = survivorsRaw.length; i < sLen; i++) {
       const perkFileName = survivorsRaw[i]
-      const key = Number(perkFileName.substr(0, 2))
+      const key = Number(perkFileName.replace(/\D+$/g, ''))
       survivors[key] = {
         index: key,
         name: perkFileName,
@@ -243,7 +243,7 @@ export default {
     // make sure array keys match the ids in file name. TODO maybe make sure no key is reassigned because of naming issues
     for (let i = 0, kLen = killersRaw.length; i < kLen; i++) {
       const perkFileName = killersRaw[i]
-      const key = Number(perkFileName.substr(0, 2))
+      const key = Number(perkFileName.replace(/\D+$/g, ''))
       killers[key] = {
         index: key,
         name: perkFileName,
